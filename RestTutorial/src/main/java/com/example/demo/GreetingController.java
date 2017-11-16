c558d2c5968caa88a3eecad7bbba786cd639445c
choch085 <33731576+choch085@users.noreply.github.com>
2017-11-16 21:12:58 +0200

    update
    import ...annotation.*;

--- RestTutorial/src/main/java/com/example/demo/GreetingController.java
+++ RestTutorial/src/main/java/com/example/demo/GreetingController.java
@@ -1,11 +1,13 @@
 package com.example.demo;
 
 import java.util.concurrent.atomic.AtomicLong;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMethod;
+//import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.bind.annotation.ResponseBody;
+//import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
